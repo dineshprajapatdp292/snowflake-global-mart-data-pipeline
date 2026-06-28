8635c13b5e94e48ac0b822b342841a2b6ca5aa1d
dineshprajapatdp292 <dineshprajapatdp292@gmail.com>
2026-06-28 17:33:46 +0530

    Create virtual warehouses for Snowflake pipeline
    GM_INGEST_WH: An XSMALL warehouse configured for automated, cost-efficient data loading (auto-suspend at 60 seconds).
    
    GM_TRANSFORM_WH: A SMALL warehouse allocated for heavier compute tasks like data transformation and cleaning throughout the pipeline layers.

--- project_code/01_setup/create_warehouse.sql
+++ project_code/01_setup/create_warehouse.sql
@@ -1,1 +1,20 @@
+-- ============================================================
+-- Project  : Snowflake Global Mart Data Pipeline
+-- Author   : Dinesh Prajapat
+-- Purpose  : Create virtual warehouses
+-- Layer    : Setup
+-- ============================================================
 
+CREATE OR REPLACE WAREHOUSE GM_INGEST_WH
+WITH
+WAREHOUSE_SIZE='XSMALL'
+AUTO_SUSPEND=60
+AUTO_RESUME=TRUE
+INITIALLY_SUSPENDED=TRUE;
+
+CREATE OR REPLACE WAREHOUSE GM_TRANSFORM_WH
+WITH
+WAREHOUSE_SIZE='SMALL'
+AUTO_SUSPEND=60
+AUTO_RESUME=TRUE
+INITIALLY_SUSPENDED=TRUE;
