93b4e310a005419cbb259ed1757ff4cb0b7890db
dineshprajapatdp292 <dineshprajapatdp292@gmail.com>
2026-06-28 17:28:51 +0530

    Update create_database.sql
    create database to store the project

--- project_code/01_setup/create_database.sql
+++ project_code/01_setup/create_database.sql
@@ -1,1 +1,10 @@
+-- ============================================================
+-- Project  : Snowflake Global Mart Data Pipeline
+-- Author   : Dinesh Prajapat
+-- Purpose  : Create the root database
+-- Layer    : Setup
+-- ============================================================
 
+CREATE OR REPLACE DATABASE global_mart_db;
+
+USE DATABASE global_mart_db;
