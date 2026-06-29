6abaa2f4d7fd04034d41c46b271f93a3dbe350bc
dineshprajapatdp292 <dineshprajapatdp292@gmail.com>
2026-06-29 22:40:51 +0530

    Create external stages for S3 data ingestion
    Added SQL commands to create external stages for S3 data ingestion, including POS Transactions, IoT Events, and ERP Orders stages.

--- project_code/02_storage/stages.sql
+++ project_code/02_storage/stages.sql
@@ -1,1 +1,34 @@
+-- ===========================================================
+-- Project : Snowflake Global Mart Data Pipeline
+-- File    : stages.sql
+-- Author  : Dinesh Prajapat
+-- Purpose : Create external stages for S3 data ingestion
+-- Layer   : Storage
+-- ===========================================================
 
+USE DATABASE global_mart_db;
+USE SCHEMA integrations;
+
+---------------------------------------------------------
+-- POS Transactions Stage (CSV)
+---------------------------------------------------------
+CREATE OR REPLACE STAGE stg_pos
+URL='s3://global-mart-db/pos/'
+STORAGE_INTEGRATION = s3_global_mart_int
+FILE_FORMAT = ff_csv;
+
+---------------------------------------------------------
+-- IoT Events Stage (JSON)
+---------------------------------------------------------
+CREATE OR REPLACE STAGE stg_iot
+URL='s3://global-mart-db/iot/'
+STORAGE_INTEGRATION = s3_global_mart_int
+FILE_FORMAT = ff_json;
+
+---------------------------------------------------------
+-- ERP Orders Stage (PARQUET)
+---------------------------------------------------------
+CREATE OR REPLACE STAGE stg_erp
+URL='s3://global-mart-db/erp/'
+STORAGE_INTEGRATION = s3_global_mart_int
+FILE_FORMAT = ff_parquet;
