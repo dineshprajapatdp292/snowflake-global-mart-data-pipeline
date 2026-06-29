e6eb7167c0f0c6bb9f4673127d5e430d9a969fb2
dineshprajapatdp292 <dineshprajapatdp292@gmail.com>
2026-06-29 22:35:32 +0530

    Create reusable file formats for data ingestion
    Added SQL commands to create reusable file formats for data ingestion.

--- project_code/02_storage/stages.sql
+++ project_code/02_storage/stages.sql
@@ -1,1 +1,26 @@
+-- ===========================================================
+-- Project : Snowflake Global Mart Data Pipeline
+-- File    : file_formats.sql
+-- Author  : Dinesh Prajapat
+-- Purpose : Create reusable file formats for data ingestion
+-- Layer   : Storage
+-- ===========================================================
 
+USE DATABASE global_mart_db;
+USE SCHEMA integrations;
+
+-- CSV File Format
+CREATE OR REPLACE FILE FORMAT ff_csv
+TYPE = 'CSV'
+FIELD_DELIMITER = ','
+SKIP_HEADER = 1
+FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+NULL_IF = ('NULL','null','');
+
+-- JSON File Format
+CREATE OR REPLACE FILE FORMAT ff_json
+TYPE = 'JSON';
+
+-- Parquet File Format
+CREATE OR REPLACE FILE FORMAT ff_parquet
+TYPE = 'PARQUET';
