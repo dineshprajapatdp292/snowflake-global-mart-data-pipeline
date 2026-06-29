cfc665f65fd3fd2430509ba1cb54f3bb3bf6b6b9
dineshprajapatdp292 <dineshprajapatdp292@gmail.com>
2026-06-29 22:41:49 +0530

    Create Snowflake Storage Integration with AWS S3
    This script creates a Snowflake storage integration for AWS S3, allowing access to specified S3 locations.

--- project_code/02_storage/storage_integration.sql
+++ project_code/02_storage/storage_integration.sql
@@ -1,1 +1,17 @@
+-- ===========================================================
+-- Project : Snowflake Global Mart Data Pipeline
+-- File    : storage_integration.sql
+-- Purpose : Create Snowflake Storage Integration with AWS S3
+-- ===========================================================
 
+USE DATABASE global_mart_db;
+USE SCHEMA integrations;
+
+CREATE OR REPLACE STORAGE INTEGRATION s3_global_mart_int
+TYPE = EXTERNAL_STAGE
+STORAGE_PROVIDER = S3
+ENABLED = TRUE
+STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::<ACCOUNT-ID>:role/snowflake-s3-role'
+STORAGE_ALLOWED_LOCATIONS = (
+'s3://global-mart-db/'
+);
