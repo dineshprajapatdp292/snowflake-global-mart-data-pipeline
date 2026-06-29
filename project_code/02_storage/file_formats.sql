49193620bd4f23211bc1d1872dff2b6c7b3808c0
dineshprajapatdp292 <dineshprajapatdp292@gmail.com>
2026-06-29 22:40:17 +0530

    Create reusable file formats for data ingestion
    Added reusable file formats for CSV, JSON, and Parquet.

--- project_code/02_storage/file_formats.sql
+++ project_code/02_storage/file_formats.sql
@@ -1,17 +1,26 @@
 -- ===========================================================
 -- Project : Snowflake Global Mart Data Pipeline
--- File    : storage_integration.sql
--- Purpose : Create Snowflake Storage Integration with AWS S3
+-- File    : file_formats.sql
+-- Author  : Dinesh Prajapat
+-- Purpose : Create reusable file formats for data ingestion
+-- Layer   : Storage
 -- ===========================================================
 
 USE DATABASE global_mart_db;
 USE SCHEMA integrations;
 
-CREATE OR REPLACE STORAGE INTEGRATION s3_global_mart_int
-TYPE = EXTERNAL_STAGE
-STORAGE_PROVIDER = S3
-ENABLED = TRUE
-STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::<ACCOUNT-ID>:role/snowflake-s3-role'
-STORAGE_ALLOWED_LOCATIONS = (
-'s3://global-mart-db/'
-);
+-- CSV File Format
+CREATE OR REPLACE FILE FORMAT ff_csv
+TYPE = 'CSV'
+FIELD_DELIMITER = ','
+SKIP_HEADER = 1
+FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+NULL_IF = ('NULL','null','');
+
+-- JSON File Format
+CREATE OR REPLACE FILE FORMAT ff_json
+TYPE = 'JSON';
+
+-- Parquet File Format
+CREATE OR REPLACE FILE FORMAT ff_parquet
+TYPE = 'PARQUET';
